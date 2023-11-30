304d1377f218f7b313742359dc430ab5dc49f44c
riecho14 <riecho14@outlook.com>
2023-11-30 12:41:21 +0700

    revisi

--- eatsy-web/resources/views/homecms/sidebar.blade.php
+++ eatsy-web/resources/views/homecms/sidebar.blade.php
@@ -30,7 +30,8 @@
             <!-- Left navbar links -->
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i
+                            class="fas fa-bars"></i></a>
                 </li>
             </ul>
 
@@ -54,8 +55,8 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="../../index3.html" class="brand-link">
-                <img src="{{ asset('assets/img/logo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-                    style="opacity: .8">
+                <img src="{{ asset('assets/img/logo.png') }}" alt="AdminLTE Logo"
+                    class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">LA Maison</span>
             </a>
 
@@ -194,6 +195,24 @@
                     "info": true,
                     "autoWidth": false,
                     "responsive": true,
+                });
+            });
+        </script>
+
+        <script>
+            $(document).ready(function() {
+                // Get the current URL
+                var currentUrl = window.location.href;
+
+                // Loop through each nav-link and compare its href attribute with the current URL
+                $('.nav-link').each(function() {
+                    var navLinkUrl = $(this).attr('href');
+
+                    if (currentUrl.includes(navLinkUrl)) {
+                        $(this).addClass('active'); // Add the 'active' class to the matching nav-link
+                    } else {
+                        $(this).removeClass('active'); // Remove the 'active' class from other nav-links
+                    }
                 });
             });
         </script>
